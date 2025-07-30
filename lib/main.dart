c5c5ed03e0245970b9609e719337c371a33e3196
minsoo-creates <ku.minsoo0314@gmail.com>
2025-07-30 17:13:00 +0900

    finale

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+//fixcomments
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: "credentials/.env");
