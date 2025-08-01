9fe9506c4e647c59f456f878db781150cfb3a2d3
minsoo-creates <ku.minsoo0314@gmail.com>
2025-08-01 19:24:47 +0900

    google maps

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-//fixcomments
+//for .env file
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
